a556f309978a4cba8fb81e07be9ca8c0b7f71ecd
AleksandrKotliar <sashagsmnew@gmail.com>
2022-12-06 16:51:33 +0400

    Initial Commit

---
+++ MyWeather2022App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather2022App
+//
+//  Created by macbook on 06.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
