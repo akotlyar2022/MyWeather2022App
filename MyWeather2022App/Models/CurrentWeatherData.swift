a3a9e76ddfc5c7aedcdc4e8ea89ad4f0d8a74068
AleksandrKotliar <sashagsmnew@gmail.com>
2022-12-07 17:51:55 +0400

    Added CurrentWeatherData with data from API Parsed the data of JSON

---
+++ MyWeather2022App/Models/CurrentWeatherData.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentWeatherData.swift
+//  MyWeather2022App
+//
+//  Created by macbook on 07.12.22.
+//
+
+import Foundation
+
+struct CurrentWeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+}
